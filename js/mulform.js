945bd7392b0b2bb8c8091f53c94c65a8f5b50650
Albert Zhivulin <albert.zhivulin@doconcall.ru>
2016-08-09 15:04:43 +0300

    dev

--- js/mulform.js
+++ js/mulform.js
@@ -2,25 +2,28 @@
     'use strict';
     
     $(function() {
-        $('form').mulform();
+        $('form').mulform({
+            test: 'test-options'
+        });
     });
     
 })(window, jQuery);
 
 
 /**
-* Bootstrap multy handler form
-* version: 1.0.0
+* Bootstrap multy form handler
+* @vers: 1.0.0
 */
 (function() {
     'use strict';
     
+    /**
+    * Init plugin
+    */
     $.fn.mulform = function(options) {
-        
         this.each(function() {
             $.data(this, 'docform', new MulForm(this, options));
         });
-        
         return this;
     };
 
@@ -28,21 +31,89 @@
     * Plugin constructor
     */
     function MulForm(form, options) {
-        this.elem = $(form);
-        
-        
-        
-        console.log(this.getElements());
-        console.log(this.elemArray);
+        this.elem                                   = $(form);
+        this.options                                = options;
+        if (this.getElements())
+            this.init();
     };
+    
+    
+    /**
+    * Init processing fields
+    */
+    MulForm.prototype.init = function()
+    {
+        this.createOptions();
+        this.treatField();
+        
+        //console.log(this.elemArray);
+        
+    };
+    
+    
+    
+    /**
+    * Treat existing field
+    */
+    MulForm.prototype.treatField = function()
+    {
+        $.each(this.elemArray, $.proxy(arrayHundler, this));
+        
+        // Array proxy hundler
+        function arrayHundler(key, elem) {
+            var element                             = $(elem);
+            var elemType                            = null;
+            
+            if (element.is('input')) {
+                elemType                            = element.attr('type');
+                
+                switch (elemType) {
+                    case 'text':
+                        console.log(elemType);
+                        break;
+                        
+                    case 'email':
+                        console.log(elemType);
+                        break;
+                        
+                    case 'password':
+                        console.log(elemType);
+                        break;
+                        
+                    case 'file':
+                        console.log(elemType);
+                        break;
+                        
+                    case 'checkbox':
+                        console.log(elemType);
+                        break;
+                        
+                    case 'radio':
+                        console.log(elemType);
+                        break;
+                }
+                
+            } else {
+                console.log('DEV | this select')
+            }
+            
+            
+            //console.log(elemType)
+            
+        };
+        
+        
+    };
+    
 
     /**
     * Get form elements
     * 
-    * Creates an array with elements
-    * return Bool
+    * Creates array with elements
+    * @return {Bool}
     */
-    MulForm.prototype.getElements = function() {
+    MulForm.prototype.getElements = function()
+    {
         // Input
         this.inputs                                 = this.elem.find('input');
         this.inputSum                               = this.inputs.length;
@@ -67,12 +138,28 @@
                 this.elemArray.push(this.selects[i])
             }
         };
- 
+        
         if (this.inputSum || this.selectSum)
             return true;
         else
             return false;
     };
+    
+    
+    
+    
+    /**
+    * Create plugin options
+    *
+    * @create {Obj}
+    */
+    MulForm.prototype.createOptions = function()
+    {
+        var options = {
+            test1: 'test'
+        };
+        this.options                                = $.extend(true, options, this.options, this.elem.data());
+    }
     
     
     
